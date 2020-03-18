5b24b8e30bd219e0c5576b7f9cf353f6697189c0
digaso <diogomoura2014@gmail.com>
2020-03-18 20:05:17 +0000

    style: cleaned some white-sapces and tabs
    closes #26

--- backend/src/controllers/ListsController.js
+++ backend/src/controllers/ListsController.js
@@ -2,19 +2,19 @@ const List = require("../models/List");
 const ParseToStringArray = require("../utils/parseStringtoArray");
 
 module.exports = {
-	async index(request, response) {
-		const lists = await List.find();
-		return response.json(lists);
-	},
-	async store(request, response) {
-		const { name, users } = request.body;
-		const usersarray = ParseToStringArray(users);
+  async index(request, response) {
+    const lists = await List.find();
+    return response.json(lists);
+  },
+  async store(request, response) {
+    const { name, users } = request.body;
+    const usersarray = ParseToStringArray(users);
 
-		const list = await List.create({
-			name,
-			users: usersarray,
-			admin_users: usersarray
-		});
-		return response.json(list);
-	}
+    const list = await List.create({
+      name,
+      users: usersarray,
+      admin_users: usersarray
+    });
+    return response.json(list);
+  }
 };
--- backend/src/index.js
+++ backend/src/index.js
@@ -2,14 +2,12 @@ const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
+require("dotenv/config");
 
-mongoose.connect(
-	"mongodb+srv://digaso:12345@scabere-r96ap.mongodb.net/test?retryWrites=true&w=majority",
-	{
-		useNewUrlParser: true,
-		useUnifiedTopology: true
-	}
-);
+mongoose.connect(process.env.DB_CONNECT, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 app.use(express.json());
 app.use(routes);
 app.listen(3333);
