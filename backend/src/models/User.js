a3458b1e0831f0ea51675ece39867ae2ccfc0868
digaso <diogomoura2014@gmail.com>
2020-03-22 18:43:13 +0000

    refactor: updated model Users
    Estructed all the model fields to be more efficient

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,12 +1,37 @@
 const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
-	name: String,
-	username: String,
-	email: String,
-	password: String,
-	photo_url: String,
-	scabs: Number,
-	friends: [String]
+  name: {
+    type: String,
+    required: true
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  photo_url: {
+    type: String,
+    default: null
+  },
+  scabs: {
+    type: Number,
+    default: 0
+  },
+  friends: {
+    type: [String],
+    default: []
+  },
+  createdat: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 module.exports = mongoose.model("User", UserSchema);
