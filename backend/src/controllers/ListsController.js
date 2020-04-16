e2e2924e83f6ba466edbd835233599f6d6515ebd
digaso <diogomoura2014@gmail.com>
2020-04-16 20:47:08 +0100

    feat(all functions in profilecontroller are made): n
    n
    
    closes #36 closes #35

--- backend/src/controllers/ListsController.js
+++ backend/src/controllers/ListsController.js
@@ -3,17 +3,41 @@ const ParseToStringArray = require("../utils/parseStringtoArray");
 
 module.exports = {
   async index(request, response) {
-    const lists = await List.find();
-    return response.json(lists);
+    try {
+      const lists = await List.find();
+      return response.json(lists);
+    } catch (error) {
+      return response.json({ error });
+    }
   },
   async store(request, response) {
-    const { name, users } = request.body;
-    const usersarray = ParseToStringArray(users);
+    const { name } = request.body;
+    const username = request.headers.username;
 
     const list = await List.create({
       name,
-      users: usersarray
+      users: [username],
+      createdby: username
     });
     return response.json(list);
+  },
+  async indexOne(request, response) {
+    const _id = request.params.id;
+    try {
+      const list = await List.find({ _id });
+      return response.status(200).json(list);
+    } catch (error) {
+      return response.json(403).json({ error });
+    }
+  },
+  async destroy(request, response) {
+    const _id = request.params.id;
+    const username = request.headers.username;
+    const list = await List.findById({ _id });
+    if (list.createdby == username) {
+      return response.status(200).json({ message: "apagou mano" });
+    } else {
+      return response.status(403).json({ error: "Isso não é teu bruh" });
+    }
   }
 };
--- backend/src/controllers/ProfileController.js
+++ backend/src/controllers/ProfileController.js
@@ -1,21 +1,34 @@
 const User = require("../models/User");
 const Task = require("../models/Task");
+const List = require("../models/List");
+const Stats = require("../models/Statistics");
 
 module.exports = {
   async index(request, response) {
     const username = request.headers.authorization;
-    console.log(username);
     const user = await User.find(username);
 
     return response.status(200).json(user);
   },
-  async userTasks(request, response) {
-    const username = request.headers.authorization;
-    console.log(username);
+  async tasks(request, response) {
+    const username = request.headers.username;
     const usertasks = await Task.find({ username });
-    console.log(usertasks);
     return response.status(200).json(usertasks);
   },
-  async userLists(request, response) {},
-  async userStats(request, response) {}
+  async lists(request, response) {
+    const username = request.headers.username;
+    const lists = await List.find({
+      users: {
+        $elemMatch: {
+          $eq: username,
+        },
+      },
+    });
+    return response.status(200).json(lists);
+  },
+  async stats(request, response) {
+    const username = request.headers.username;
+    const stats = await List.find({ username });
+    return response.status(200).json(stats);
+  },
 };
