af1c8691aa98bfc443e3f217340120d46067aebd
digaso <diogomoura2014@gmail.com>
2020-05-19 22:56:32 +0100

    fix(frontend): when Logged in user passes to home page
    y
    
    closes #41

--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -1,22 +1,35 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-import Home from "./components/pages/Home";
+import StartScreen from "./components/pages/StartScreen";
+import Profile from "./components/pages/Profile";
 import Login from "./components/pages/Login";
 import Register from "./components/pages/Register";
 import CheckAge from "./components/pages/CheckAge";
+import Home from "./components/pages/Home";
 
 const AppStack = createStackNavigator();
+const AppTab = createBottomTabNavigator();
 
+function Main() {
+	return (
+		<AppTab.Navigator>
+			<AppTab.Screen name="Profile" component={Profile} />
+			<AppTab.Screen name="Home" component={Home} />
+		</AppTab.Navigator>
+	);
+}
 export default function Routes() {
 	return (
 		<NavigationContainer>
 			<AppStack.Navigator screenOptions={{ headerShown: false }}>
-				<AppStack.Screen name="Home" component={Home} />
+				<AppStack.Screen name="StartScreen" component={StartScreen} />
 				<AppStack.Screen name="Register" component={Register} />
 				<AppStack.Screen name="Login" component={Login} />
 				<AppStack.Screen name="CheckAge" component={CheckAge} />
+				<AppStack.Screen name="Main" component={Main} />
 			</AppStack.Navigator>
 		</NavigationContainer>
 	);
