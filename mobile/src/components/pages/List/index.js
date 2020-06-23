801fa010a557f0c4473ea7b7c37f299b25abef6c
digaso <diogomoura2014@gmail.com>
2020-06-23 16:31:10 +0100

    feat(frontend): added single List screen
    Theres is multiple errors in the animation that will be corrected in the next commit form this
    document

--- mobile/src/components/pages/List/index.js
+++ mobile/src/components/pages/List/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useContext } from "react";
+import React, { useEffect, useState, useContext, useRef } from "react";
 import {
 	View,
 	Text,
@@ -8,51 +8,133 @@ import {
 	AsyncStorage,
 	Dimensions,
 	Image,
+	Animated,
 } from "react-native";
 import PlusImage from "../../../../assets/add_circle-24px.png";
-import mainContext from "../../../services/mainContext";
-import listContext from "../../../services/listContext";
+import mainContext from "../../../services/contexts/mainContext";
+import listContext from "../../../services/contexts/listContext";
 import { MaterialIcons } from "@expo/vector-icons";
 import styles from "./styles";
+import { Modal } from "react-native-paper";
+import { Form } from "unform";
+import Input from "../../utils/Input";
 
 export default function List({ route, navigation }) {
-	const { idlist, clean, tasks, getTasksList, edited } = useContext(
-		listContext
+	const {
+		idlist,
+		clean,
+		tasks,
+		getTasksList,
+		edited,
+		toogleEdited,
+		deleteList,
+	} = useContext(listContext);
+	const colors = useRef(
+		Array.from({ length: tasks.length }).fill(new Animated.Value(0))
 	);
+	const [visible, setVisible] = useState(false);
 	const [listName, setListName] = useState("");
 	const screenWidth = Math.round(Dimensions.get("window").width);
 	const screenHeight = Math.round(Dimensions.get("window").height);
+	const formRef = useRef(null);
 
 	async function getListName() {
 		setListName(await AsyncStorage.getItem("listName"));
 	}
-
+	async function asyncGetTasks() {
+		await getTasksList();
+		console.log(tasks);
+	}
 	useEffect(() => {
 		getListName();
-		getTasksList();
-		console.log(tasks);
+		asyncGetTasks();
 	}, [edited]);
 	return (
 		<View style={styles.container}>
 			<StatusBar hidden />
-			<TouchableOpacity>
-				<MaterialIcons
-					name="arrow-back"
-					size={32}
+			<View style={styles.buttonsContainer}>
+				<TouchableOpacity
 					onPress={() => {
 						clean();
 						navigation.goBack();
 					}}
-				/>
-			</TouchableOpacity>
+				>
+					<MaterialIcons name="arrow-back" size={32} />
+				</TouchableOpacity>
+				<TouchableOpacity
+					onPress={() => {
+						deleteList();
+						clean();
+						navigation.goBack();
+					}}
+				>
+					<MaterialIcons name="delete" size={32} color="#bc0000" />
+				</TouchableOpacity>
+			</View>
 			<View style={styles.titleContent}>
 				<Text style={styles.titleText}>{listName}</Text>
 			</View>
-			<ScrollView>
-				{tasks.length > -1 ? (
-					<TouchableOpacity key={0} style={styles.task}>
-						<Text style={styles.taskText}>324</Text>
-					</TouchableOpacity>
+			<ScrollView style={{ height: screenHeight }}>
+				{tasks.length > 0 ? (
+					tasks.map((item, index) => (
+						<TouchableOpacity key={index} style={styles.task}>
+							<View style={{ flexDirection: "row" }}>
+								<TouchableOpacity
+									style={{ alignSelf: "center", marginRight: 8 }}
+									onPress={() => {
+										console.log(colors.current[index]);
+
+										Animated.timing(colors.current[index], {
+											toValue: 1,
+											duration: 1000,
+										}).start();
+									}}
+								>
+									<Animated.View
+										style={{
+											borderColor: "#000",
+											borderWidth: 3,
+											borderRadius: 100,
+										}}
+									>
+										<Animated.View
+											style={{
+												backgroundColor: "#000",
+												height: 22,
+												width: 22,
+												borderRadius: 100,
+												opacity: colors.current[index],
+											}}
+											nativeID={item._id}
+										></Animated.View>
+									</Animated.View>
+								</TouchableOpacity>
+								<View>
+									<Text style={styles.taskText}>{item.title}</Text>
+								</View>
+							</View>
+
+							<Animated.View
+								style={{
+									position: "absolute",
+									alignItems: "center",
+									alignContent: "center",
+									opacity: 0.5,
+									alignSelf: "center",
+								}}
+							>
+								<Text
+									style={{
+										color: "#000",
+										opacity: 0,
+										fontSize: 32,
+									}}
+								>
+									Done!!
+								</Text>
+							</Animated.View>
+						</TouchableOpacity>
+					))
 				) : (
 					<View style={styles.emptyContent}>
 						<Text style={styles.emptyText}>This list don't have tasks yet</Text>
@@ -68,7 +150,7 @@ export default function List({ route, navigation }) {
 					width: 50,
 					alignSelf: "flex-end",
 				}}
-				onPress={() => setVisible(true)}
+				onPress={() => navigation.navigate("NewTask")}
 			>
 				<Image source={PlusImage} />
 			</TouchableOpacity>
--- mobile/src/components/pages/List/styles.js
+++ mobile/src/components/pages/List/styles.js
@@ -1,12 +1,17 @@
 import React from "react";
 import { StyleSheet } from "react-native";
 export default StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+	},
 	titleContent: {
 		alignContent: "center",
 		alignItems: "center",
+		marginBottom: 8,
 	},
 	titleText: {
-		fontSize: 30,
+		fontSize: 32,
 	},
 	emptyContent: {
 		marginTop: 10,
@@ -15,5 +20,56 @@ export default StyleSheet.create({
 		fontSize: 24,
 		color: "#9a9aa9",
 		alignSelf: "center",
+	},
+	buttonsContainer: {
+		flexDirection: "row",
+		justifyContent: "space-between",
+		marginHorizontal: 10,
+		marginVertical: 10,
+	},
+	task: {
+		flex: 1,
+		justifyContent: "flex-start",
+		marginVertical: 8,
+		marginHorizontal: 8,
+		backgroundColor: "#eee",
+		paddingVertical: 8,
+		paddingLeft: 8,
+		borderRadius: 4,
+		shadowOffset: {
+			width: 3,
+			height: 3,
+		},
+		shadowColor: "#000",
+		shadowOpacity: 0.2,
+	},
+	taskText: {
+		fontSize: 18,
+	},
+	modal: {
+		backgroundColor: "rgba(0,0,0,0.3)",
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	modalContent: {
+		flexDirection: "column",
+		alignItems: "center",
+		backgroundColor: "#eee",
+		width: 300,
+		height: 100,
+		borderRadius: 7,
+	},
+	modalButtons: {
+		flexDirection: "row",
+		justifyContent: "space-evenly",
+	},
+	textCreateButton: {
+		color: "rgba(0,30,255,0.5)",
+		fontWeight: "600",
+	},
+	textCancelButton: {
+		color: "rgba(255,0,0,0.5)",
+		fontWeight: "600",
 	},
 });
